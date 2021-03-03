33db8449fa787dc0cf1249c92e2c7b97c2e0c0b0
christophvogel <christoph@boerd.net>
2021-03-03 17:35:33 +0100

    ✨ Implement CR(UD) Operators

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,14 @@
-import { printWelcomeMessage, printNoAccess } from "./messages";
-import { askForAction, askForCredentials } from "./questions";
-import { handleGetPassword, handleSetPassword, hasAccess } from "./commands";
-import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import { closeDB, connectDB, createPasswordDoc, getCollection } from "./db";
 dotenv.config();
 
 const run = async () => {
   const url = process.env.MONGODB_URL;
 
   try {
-    const client = await MongoClient.connect(url, {
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to DB!");
-
-    const db = client.db("PW-Manager-Christoph");
-
-    await db.collection("inventory").insertOne({
-      item: "Ball",
-      qty: 566,
-      tags: ["leather"],
-      size: { h: 10, w: 10 },
-    });
-
-    client.close();
+    await connectDB(url, "PW-Manager-Christoph");
+    await createPasswordDoc({ name: "Christoph", value: "1111" });
+    await closeDB();
   } catch (error) {
     console.error(error);
   }
---
+++ src/db.ts
@@ -0,0 +1,32 @@
+import { Collection, Db, MongoClient } from "mongodb";
+
+let client: MongoClient = null;
+let db: Db = null;
+
+export type PasswordDoc = {
+  name: string;
+  value: string;
+};
+
+export async function connectDB(url: string, dbName: string) {
+  client = await MongoClient.connect(url, { useUnifiedTopology: true });
+  db = client.db(dbName);
+}
+
+export function getCollection<T>(collectionName: string): Collection<T> {
+  return db.collection<T>(collectionName);
+}
+
+export function closeDB() {
+  client.close();
+}
+
+export async function createPasswordDoc(passwordDoc: PasswordDoc) {
+  const passwordCollection = await getCollection<PasswordDoc>("passwords");
+  return await passwordCollection.insertOne(passwordDoc);
+}
+
+export async function readPasswordDoc(passwordName: string) {
+  const passwordCollection = await getCollection<PasswordDoc>("passwords");
+  return await passwordCollection.findOne({ name: passwordName });
+}
